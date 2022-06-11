2ab5867d4a80fb85b0050cd0996e45c063c8e90f
francisco-solis99 <franciscosoliscss@gmail.com>
2022-06-11 09:39:00 -0500

    [Add] the initial structure since JS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
-import './style.css'
+const app = document.getElementById('app');
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// create the structure and the html elements 🟩
+const ticTacTocBoard = document.createElement('div');
+ticTacTocBoard.classList.add('tic-tac-toe__board');
+const CELLS = 9;
+// const board = Array(CELLS).fill(0);
+// console.log(board);
+
+const cell = posNumCell => {
+  return `
+    <div class="tic-tac-toe__cell" data-position="${posNumCell}">
+    </div>
+  `;
+};
+
+const genCells = (numCells = 9) => {
+  const cells = [];
+  for (let i = 0; i < numCells; i += 1) cells.push(cell(i));
+  return cells.join('');
+};
+
+ticTacTocBoard.innerHTML = `
+  ${genCells(CELLS)}
+`;
+
+// render the cells
+app.appendChild(ticTacTocBoard);
+
+// begin the game 🟩
+
+const attempt = () => {
+  return `
+    <img src="./images/cross.svg" alt="cross for tic tac toe">
+  `;
+};
+
+ticTacTocBoard.addEventListener('click', (e) => {
+
+});
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default ({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    assetsDir: './'
+  },
+  server: {
+    port: 3030
+  }
+});
