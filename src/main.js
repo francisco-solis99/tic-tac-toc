626ca8c2b58e17c29a9315ff32beadd3ba4aa2f2
francisco-solis99 <franciscosoliscss@gmail.com>
2022-06-11 12:26:31 -0500

    [Add] the data structure and do the logic to know if the player won

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 const app = document.getElementById('app');
-
 // create the structure and the html elements 🟩
 const ticTacTocBoard = document.createElement('div');
 ticTacTocBoard.classList.add('tic-tac-toe__board');
 const CELLS = 9;
-// const board = Array(CELLS).fill(0);
-// console.log(board);
+const board = Array(CELLS).fill(null);
+let player1Turn = true;
 
 const cell = posNumCell => {
   return `
@@ -29,12 +28,40 @@ app.appendChild(ticTacTocBoard);
 
 // begin the game 🟩
 
-const attempt = () => {
+const attempt = (turn) => {
   return `
-    <img src="./images/cross.svg" alt="cross for tic tac toe">
+    <img src="./images/${turn}.svg" class="tic-tac-toe__image" alt="${turn} for tic tac toe">
   `;
 };
 
 ticTacTocBoard.addEventListener('click', (e) => {
+  if (!(e.target.classList.contains('tic-tac-toe__cell'))) return;
 
+  const cell = e.target;
+  const turn = player1Turn ? 'cross' : 'circle';
+  const position = +cell.dataset.position;
+  cell.innerHTML = attempt(turn);
+  board[position] = turn;
+  player1Turn = !player1Turn;
+  if (board.filter(item => item !== null).length > 4) {
+    checkMove(turn, cell.dataset.position);
+  }
 });
+
+function checkMove (turn, pos) {
+  const WAYS_TO_WIN = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]];
+  const waysFiltered = WAYS_TO_WIN.filter(element => element.includes(+pos));
+  const currentBoard = board.map((cell, index) => cell === turn ? index : null).filter(item => item !== null);
+  const isWin = waysFiltered.some(way => {
+    let times = 0;
+    currentBoard.forEach(pos => {
+      if (way.includes(pos)) times += 1;
+    });
+    return times === 3;
+  });
+  if (isWin) {
+    console.log('El jugador ha ganado');
+  }
+}
